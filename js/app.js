8a39e5f36b253f891fc70364afbfdc1bb043863a
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-05-16 08:22:04 -0400

    created variables + date js.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,21 @@
 // CODE EXPLAINED channel
 
+// Select the Elements
+const clear = document.querySelector(".clear")
+const dateElement = document.getElementById("date")
+const list = document.getElementById("list")
+const input = document.getElementById("input")
+
+// Classes names
+const CHECK = "fa-check-circle"
+const UNCHECK = "fa-circle-thin"
+const LINE_THROUGH = "lineThrough"
+
+// Show todays data
+const options = {weekday : "long", month:"short", day:"numeric"}
+const today = new Data()
+
+dateElement.innerHTML = today.toLocaleDataString("en-US", options)
+
+
+
