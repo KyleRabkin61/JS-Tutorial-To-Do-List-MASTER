9138cabfa0dabc48d7a89d3e2a1f183b34558874
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-05-16 08:54:27 -0400

    okay java is starting to work

--- js/app.js
+++ js/app.js
@@ -11,11 +11,54 @@ const CHECK = "fa-check-circle"
 const UNCHECK = "fa-circle-thin"
 const LINE_THROUGH = "lineThrough"
 
+// Variables
+let LIST, id
+
 // Show todays data
 const options = {weekday : "long", month:"short", day:"numeric"}
-const today = new Data()
+const today = new Date()
 
-dateElement.innerHTML = today.toLocaleDataString("en-US", options)
+dateElement.innerHTML = today.toLocaleDateString("en-US", options)
 
+// Add to do function
+function addToDo(toDo, id, done, trash) {
+
+    if(trash){return}
+
+    const DONE = done ? CHECK : UNCHECK
+    const LINE = done ? LINE_THROUGH : ""
+
+    const item = `
+    <li class="item">
+        <i class="fa ${DONE} co" job="complete" id="0"></i>
+        <p class="text ${LINE}">${toDo}</p>
+        <i class="fa fa-trash-o de" job="delete" id="0"></i>
+    </li>
+    `
+    const positon = "beforeend"
+
+    list.insertAdjacentHTML(positon, item)
+}
+
+// add an item to the list user the enter key
+document.addEventListener("keyup", function(even){
+    if(event.keyCode === 13){
+        const toDo = input.value
+
+        // if the input isn't empty
+        if(toDo) {
+            addToDo(toDo, id, false, false)
+            LIST.push({
+                name : toDo,
+                id : id,
+                done : false,
+                trash : false
+            })
+
+            id++
+        }
+        input.value = ""
+    }
+})
 
 
